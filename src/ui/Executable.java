9c3daea6f3a100220f19558ea8998cb0c16a6b7f
jujoroco06 <juanjoseroco06@gmail.com>
2025-05-01 17:28:39 -0500

    Con constructores

--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -1,4 +1,14 @@
 package src.ui;
 
+import src.model.Controller;
+
+import java.util.Scanner;
+
 public class Executable {
+    private Scanner reader;
+    private Controller controller;
+    public Executable() {
+        this.reader = new Scanner(System.in);
+        this.controller = new Controller();
+    }
 }
